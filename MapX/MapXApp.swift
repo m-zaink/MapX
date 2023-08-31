7943fd2d102d9f1951a826d86874c17fa712c576
Mohammed Sadiq <mzainkofficial@gmail.com>
2023-08-31 10:30:10 +0530

    Initial Commit

---
+++ MapX/MapXApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapXApp.swift
+//  MapX
+//
+//  Created by Tokyo on 31/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MapXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
